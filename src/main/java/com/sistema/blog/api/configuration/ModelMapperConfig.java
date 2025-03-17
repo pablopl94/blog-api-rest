8c3cf313bc4ae74992787e08c33452cebaadf2ba
Pablo <pabloprietolopez94@outlook.es>
2025-03-17 17:57:06 +0100

    Creamos la clase ModelMapperConfig y configuramos un Bean para ModelMapper

---
+++ src/main/java/com/sistema/blog/api/configuration/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.sistema.blog.api.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+	    @Bean
+	    ModelMapper modelMapper() {
+	        ModelMapper modelMapper = new ModelMapper();
+	        return modelMapper;
+	    }
+	}
